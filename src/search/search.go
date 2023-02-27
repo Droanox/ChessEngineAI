750a03b333a1dc36e6f19506b2ffa621d26b87af
Droanox <50217553+Droanox@users.noreply.github.com>
2023-02-27 19:00:01 +0000

    ordered file, added PV ordering

--- src/search/search.go
+++ src/search/search.go
@@ -5,129 +5,41 @@ import (
 	"math"
 
 	"github.com/Droanox/ChessEngineAI/src/board"
-	"github.com/Droanox/ChessEngineAI/src/engine"
 )
 
 func Search(depth int, cb *board.ChessBoard) {
+	// reset nodes counter
 	nodes = 0
-	var score int = negamax(math.MinInt32, math.MaxInt32, depth, cb)
-	fmt.Printf("info depth %d nodes %d score cp %d\n", depth, nodes, score)
 
+	// reset killer moves and history moves
+	killerMoves = [2][board.MaxPly]board.Move{}
+	historyMoves = [12][board.MaxPly]int{}
+
+	// reset principal variation
+	pvTable = [board.MaxPly][board.MaxPly]board.Move{}
+	pvLength = [board.MaxPly]int{}
+	pvFollowed = false
+
+	for currDepth := 1; currDepth <= depth; currDepth++ {
+		// follow principal variation
+		pvFollowed = true
+
+		// perform negamax search
+		var score int = negamax(math.MinInt32, math.MaxInt32, currDepth, cb)
+
+		// print principal variation
+		fmt.Printf("info depth %d nodes %d score cp %d pv ", currDepth, nodes, score)
+		for i := 0; i < pvLength[board.Ply+1]; i++ {
+			PrintMove(pvTable[0][i])
+			fmt.Print(" ")
+		}
+		fmt.Println()
+	}
+
+	// print best move
 	fmt.Printf("bestmove ")
-	PrintMove(bestMove)
+	PrintMove(pvTable[0][0])
 	fmt.Println()
-}
-
-func negamax(alpha int, beta int, depth int, cb *board.ChessBoard) int {
-	if depth == 0 {
-		return quiescence(alpha, beta, cb)
-	}
-	nodes++
-
-	var isChecked bool
-	if board.SideToMove == board.White {
-		isChecked = cb.IsSquareAttackedBySide(board.BitScanForward(cb.WhiteKing), board.Black)
-	} else {
-		isChecked = cb.IsSquareAttackedBySide(board.BitScanForward(cb.BlackKing), board.White)
-	}
-	if isChecked {
-		depth++
-	}
-
-	var oldAlpha int = alpha
-	var bestMoveSoFar board.Move
-	var legalMovesNum int
-
-	var moveList = []board.Move{}
-	cb.GenerateMoves(&moveList)
-
-	scoreMoves(&moveList)
-
-	for i := 0; i < len(moveList); i++ {
-		pickMove(&moveList, i)
-
-		if !cb.MakeMove(moveList[i]) {
-			continue
-		}
-		legalMovesNum++
-		var score int = -negamax(-beta, -alpha, depth-1, cb)
-		cb.MakeBoard()
-
-		// fail-hard
-		if score >= beta {
-			if (moveList[i].GetMoveFlags() & board.MoveCaptures) == 0 {
-				killerMoves[1][board.Ply+1] = killerMoves[0][board.Ply+1]
-				killerMoves[0][board.Ply+1] = moveList[i]
-			}
-
-			return beta
-		}
-		// found a better move
-		if score > alpha {
-			if (moveList[i].GetMoveFlags() & board.MoveCaptures) == 0 {
-				historyMoves[moveList[i].GetMoveStartPiece()+(6*board.SideToMove)-1][moveList[i].GetMoveEnd()] += depth
-			}
-
-			alpha = score
-
-			if board.Ply == -1 {
-				bestMoveSoFar = moveList[i]
-			}
-		}
-	}
-
-	if legalMovesNum == 0 {
-		if isChecked {
-			return (math.MinInt32 + 10) + board.Ply
-		} else {
-			return 0
-		}
-	}
-	if oldAlpha != alpha {
-		bestMove = bestMoveSoFar
-	}
-
-	// fails low
-	return alpha
-}
-
-func quiescence(alpha int, beta int, cb *board.ChessBoard) int {
-	nodes++
-	var standPat int = engine.Eval(*cb)
-	// fail-hard
-	if standPat >= beta {
-		return beta
-	}
-	// found a better move
-	if alpha < standPat {
-		alpha = standPat
-	}
-
-	var moveList = []board.Move{}
-	cb.GenerateCaptures(&moveList)
-
-	scoreMoves(&moveList)
-
-	for i := 0; i < len(moveList); i++ {
-		pickMove(&moveList, i)
-
-		if !cb.MakeCapture(moveList[i]) {
-			continue
-		}
-		var score int = -quiescence(-beta, -alpha, cb)
-		cb.MakeBoard()
-
-		// fail-hard
-		if score >= beta {
-			return beta
-		}
-		// found a better move
-		if score > alpha {
-			alpha = score
-		}
-	}
-	// fails low
-	return alpha
 }
 
 func PrintMove(move board.Move) {
