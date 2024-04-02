1b0031cb4c2a025f35b55542eebd5a64100d7f1e
Droanox <leonlivkol@gmail.com>
2024-04-02 13:56:53 +0100

    Fixed node count
    Was testing out fail condition, accidently left the incorrect node count in.

--- src/board/perft_test.go
+++ src/board/perft_test.go
@@ -43,7 +43,7 @@ var perftTests = []struct {
 		Name:  "Wiki Position 5",
 		FEN:   "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
 		depth: 5,
-		nodes: 89941195,
+		nodes: 89941194,
 	}, {
 		Name:  "Wiki Position 6",
 		FEN:   "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
